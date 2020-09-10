b147af24140fdb76aff36a8fb26f9cf9bd004847
Ryan-Riaz <devryanriaz@gmail.com>
2020-09-10 16:17:17 +0600

    slider has done!

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// console.log("connected");
+
+$(document).ready(function () {
+	$(".slider").slick({
+		autoplay: true,
+		arrows: false,
+		dots: true,
+		appendDots: ".slider-dots",
+		fade: true,
+	});
+});
